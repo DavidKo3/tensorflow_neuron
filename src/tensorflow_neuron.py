a1fe576d1197d06311f62cab9642da3b12100db0
tensor <tensor@tensor-VirtualBox>
2016-11-02 11:57:10 +0900

    tensorflow_neuron_ex

--- src/tensorflow_neuron.py
+++ src/tensorflow_neuron.py
@@ -13,3 +13,14 @@ train_step  = tf.train.GradientDescentOptimizer(0.025).minimize(loss)
 for value in [x, w, y, y_,loss]:
     tf.scalar_summary(value.op.name, value)
     
+summaries = tf.merge_all_summaries()
+
+sess = tf.Session()
+summary_writer = tf.train.SummaryWriter('log_simple_stats', sess.graph)
+sess.run(tf.initialize_all_variables())
+
+for i in range(150):
+    if i % 10 ==0:
+        print("epch {}, output : {} ".format(i, sess.run(y)))
+    summary_writer.add_summary(sess.run(summaries), i)
+    sess.run(train_step)
